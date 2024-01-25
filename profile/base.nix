db06e1fcfc42e3f1cf531d78dee4275faa9845ec
Lyle Cleveland <lyle.cleveland@yahoo.com>
2024-01-25 11:19:49 -0600

    add wm

--- profile/base.nix
+++ profile/base.nix
@@ -7,6 +7,7 @@
     ../system/time.nix
     ../system/sound.nix
     ../app/neovim.nix
+    ../system/wm/kde_plasma.nix
   ];
   nix.settings.experimental-features = [ nix-command flakes ];
   nixpkgs.config.allowUnfree = true;
