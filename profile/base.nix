287b2892a6e9fd83858e96ab3a4f2cd8980e61ac
Lyle Cleveland <lyle.cleveland@yahoo.com>
2024-01-25 11:34:19 -0600

    add system

--- profile/base.nix
+++ profile/base.nix
@@ -10,6 +10,7 @@
     ../system/wm/kde_plasma.nix
     ../user/lcleveland.nix
   ];
+  nixpkgs.hostPlatform.system = "x86_64-linux";
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nixpkgs.config.allowUnfree = true;
 }
