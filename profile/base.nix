b042681dab128ffd14775f553d04a3e292a3175d
Lyle Cleveland <lyle.cleveland@yahoo.com>
2024-01-25 11:41:01 -0600

    test

--- profile/base.nix
+++ profile/base.nix
@@ -1,16 +1,18 @@
 { config, pkgs, ... }:
 {
-  modules = [
-    ../system/boot.nix
-    ../system/locale.nix
-    ../system/networking.nix
-    ../system/time.nix
-    ../system/sound.nix
-    ../app/neovim.nix
-    ../system/wm/kde_plasma.nix
-    ../user/lcleveland.nix
-  ];
-  nixpkgs.hostPlatform.system = "x86_64-linux";
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  nixpkgs.config.allowUnfree = true;
+  outputs = {
+    modules = [
+      ../system/boot.nix
+      ../system/locale.nix
+      ../system/networking.nix
+      ../system/time.nix
+      ../system/sound.nix
+      ../app/neovim.nix
+      ../system/wm/kde_plasma.nix
+      ../user/lcleveland.nix
+    ];
+    nixpkgs.hostPlatform.system = "x86_64-linux";
+    nix.settings.experimental-features = [ "nix-command" "flakes" ];
+    nixpkgs.config.allowUnfree = true;
+  };
 }
