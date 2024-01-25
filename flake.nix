f32be056f6ee09a357dd02d9608d7620fef09b19
Lyle Cleveland <lyle.cleveland@yahoo.com>
2024-01-25 11:42:03 -0600

    set system

--- flake.nix
+++ flake.nix
@@ -9,6 +9,7 @@
     nixosConfigurations = {
       nixos =
         nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
           specialArgs = inputs;
           modules = [ ./profile/base.nix ];
         };
