33a09a1a14eb73e2f23cda51a396878c8f3af619
Lyle Cleveland <lyle.cleveland@yahoo.com>
2024-01-26 08:27:09 -0600

    fix commit

--- system/app/auto_cpufreq.nix
+++ system/app/auto_cpufreq.nix
@@ -1,10 +1,4 @@
 { config, pkgs, ... }:
 {
-  inputs = {
-    auto-cpufreq = {
-      url = "github:AdnanHodzic/auto-cpufreq";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
   programs.auto-cpufreq.enable = true;
 }
